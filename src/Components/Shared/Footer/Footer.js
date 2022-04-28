b9332e3f590f841f6219b77c175389ade9e961da
Asadujjaman Julhas <fahim0760@gmail.com>
2022-04-29 00:42:44 +0600

    Added react router active link

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -17,8 +17,6 @@ const Footer = () => {
            +880 172 44 11 565 <br/>
            +(235) 472 9087
          </p>
-         <p>Opening hours:</p>
-         <p> Sun - Thus: 9:00 - 18:00</p>
        </div>
       </div> 
       <div className="col-md-4 col-sm-6">
@@ -31,23 +29,13 @@ const Footer = () => {
              <li><a href="#">&gt;  Privicy &amp; Policy</a></li>
              <li><a href="#">&gt;  Condition &amp; Terms</a></li>
            </ul>
-          <div className="footer_social">
-            <p>Follow us on:</p>
-             <a href=""><i className="fa fa-facebook" aria-hidden="true"></i></a>
-             <a href=""><i className="fa fa-twitter" aria-hidden="true"></i></a>
-             <a href=""><i className="fa fa-google" aria-hidden="true"></i></a>
-             <a href=""><i className="fa fa-youtube" aria-hidden="true"></i></a>
-             <a href=""><i className="fa fa-linkedin" aria-hidden="true"></i></a>
-          </div>
+          
          </div>
         </div>
         <div className="col-md-4 col-sm-12">
          <div className="contactus_area">
            <h3>Quick Contact us</h3>
            <form id="main-contact-form" name="contact-form" method="post" action="">
-            <div className="form-group">
-              <input type="text" name="name" className="form-control" placeholder="Name" required="required"/>
-            </div>
             <div className="form-group">
               <input type="email" name="email" className="form-control" placeholder="Email Address" required="required"/>
             </div>
